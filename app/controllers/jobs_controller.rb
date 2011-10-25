150451b9c325358be2be39351e986e1cdb2dc175
Tim James <tjames@tjames.com>
2011-10-25 08:52:53 -0700

    simple access control: forbid editing and deleting jobs not created by current_user

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :show]
+  before_filter :forbid_unless_job_belongs_to_current_user!, :only => [:edit, :update, :destroy]
   
   respond_to :html, :json
   
@@ -11,4 +12,13 @@ class JobsController < ApplicationController
     @job.user = current_user
     create!
   end
+  
+  # TODO: should be in module JobsHelper?
+  def forbid_unless_job_belongs_to_current_user!
+    return true if user_signed_in? and Job.find(params[:id]).try(:user) == current_user
+    flash[:notice] = "User does not own this job; access denied"
+    head :forbidden
+    false
+  end
+  
 end
--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -30,6 +30,24 @@ describe JobsController do
       end
     end
   
+    describe "forbid editing and deleting jobs not created by current_user" do
+      let(:user2) { Factory.create(:user, email: 'user2@example.com') }
+      let(:job)   { Factory.create(:job, user: user2) }
+      let(:job_edited_params) { Factory.attributes_for(:job).merge(title:'wonky!')}
+      
+      before { job }
+      after do
+        response.status.should == 403  # TODO: what should the user actually see? This case should never happen in normal use, but isn't impossible; we should show something
+        Job.all.should == [job]
+        Job.first.title.should_not == 'wonky!'
+        Job.first.title.should == Factory.attributes_for(:job)[:title]
+      end
+
+      it { get :edit, :id => job }
+      it { put :update, :id => job, :job => job_edited_params }
+      it { delete :destroy, :id => job }
+    end
+  
   end
 
 end
