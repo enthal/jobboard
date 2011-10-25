750a4b41d4de5ded626461ebe20f58eac0d6b841
Tim James <tjames@tjames.com>
2011-10-25 00:30:22 -0700

    edit job view

--- spec/views/jobs/edit.html.haml_spec.rb
+++ spec/views/jobs/edit.html.haml_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe "jobs/new.html.haml" do
+describe "jobs/edit.html.haml" do
   before do
     assign :job, Factory.create(:job)
   end
 
-  it "renders new job form" do
+  it "renders edit job form" do
     render
 
-    assert_select "form", :action => jobs_path, :method => "post" do
+    assert_select "form", :action => jobs_path(@job), :method => "post" do
       assert_select "input#job_title", :name => "job[title]"
       assert_select "input#job_company", :name => "job[company]"
       assert_select "textarea#job_description", :name => "job[description]"
