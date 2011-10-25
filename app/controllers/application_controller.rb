5bf7b44ef2ddb7b3eaed401f2153ce76a70cf491
Tim James <tjames@tjames.com>
2011-10-24 17:21:59 -0700

    in ApplicationController: inherit_resources  # https://github.com/josevalim/inherited_resources  mixes in boilerplate CRUD code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  inherit_resources  # https://github.com/josevalim/inherited_resources  mixes in boilerplate CRUD code
   protect_from_forgery
 end
