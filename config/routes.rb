e6dda277ca02826b7659c4f535cbb0c7d193f4d8
Tim James <tjames@tjames.com>
2011-10-24 17:42:48 -0700

    rails generate controller jobs index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Jobboard::Application.routes.draw do
+  get "jobs/index"
+
   devise_for :users
+  
+  resources :jobs
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +54,7 @@ Jobboard::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => redirect('/jobs')
 
   # See how all your routes lay out with "rake routes"
 
