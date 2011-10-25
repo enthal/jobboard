45727b5c024179fe4a3704520531e24dcb1c3b0e
Tim James <tjames@tjames.com>
2011-10-24 18:06:38 -0700

    all fields in jobs list (TDD)

--- spec/views/jobs/index.html.haml_spec.rb
+++ spec/views/jobs/index.html.haml_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe "jobs/index.html.haml" do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  include Devise::TestHelpers
+  
+  let(:jobs) { [Factory.create(:job)] * 2 }
+  before(:each) { assign :jobs, jobs }
+
+  it "renders a list of jobs" do
+    render
+    Factory.attributes_for(:job).each_value do |value|
+      assert_select "tr>td", :text => value.to_s, :count => 2
+    end
+  end
+  
 end
